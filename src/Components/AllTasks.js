f552cf0436a245b944cf2c58d7613b2c6ca6ba2e
Joshua Welsh <j22yosh@gmail.com>
2022-08-22 07:32:14 -0400

    working on the listing component

---
+++ src/Components/AllTasks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function AllTasks() {
+  return (
+    <div>
+    <header>
+    
+      <form>
+        <input type='text' />
+        <input type='text' />
+        <input type='text' />
+        
+           
+        
+      </form>
+      </header>
+          </div>
+  );
+}
+
+export default AllTasks
